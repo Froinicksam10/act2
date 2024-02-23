34deb42920fe56f76a45065134bc86cca1bc7561
Cirde <cirdemosquito04@gmail.com>
2024-02-23 20:59:23 +0800

    activity2

--- myactivity2/routes/web.php
+++ myactivity2/routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/aboutus', function () {
+    return view('aboutus');
+});
+
+Route::get('/service', function () {
+    return view('service');
+});
